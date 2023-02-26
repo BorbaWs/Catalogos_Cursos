4eaa014ad4b767064453ea8a42dd3b4b7688ccbf
BorbaWs <wsborba@gmail.com>
2023-02-26 13:44:37 -0300

    Catalogo de Cursos

---
+++ data/cursos.php
@@ -0,0 +1,26 @@
+<?php
+
+function cursos() {
+    return [
+        "Emotional Intelligence" => [
+            "nome_curso" => "Curso Emotional Intelligence",
+            "carga_horaria" => 2,
+            "status" => true,
+        ],
+        "Gestão do Tempo" => [
+            "nome_curso" => "Curso Gestão do Tempo",
+            "carga_horaria" => 8,
+            "status" => true,
+        ],
+        "Conceito de Produtividade" => [
+            "nome_curso" => "Curso Conceito de Produtividade",
+            "carga_horaria" => 4,
+            "status" => true,
+        ],
+        "Práticas de Empreendedorismo" => [
+            "nome_curso" => "Práticas de Empreendedorismo",
+            "carga_horaria" => 12,
+            "status" => true,
+        ],
+    ];
+}
---
+++ index.php
@@ -0,0 +1,5 @@
+<?php
+
+require_once "issues/formatador.php";
+require_once "data/cursos.php";
+require_once "ui/main.php";
